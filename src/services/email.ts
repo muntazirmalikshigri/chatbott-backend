6751ae834d8beaa9387e6787643dfcfd86d4b3a0
muntazirmalikshigri <muntazirshigri.12@gmail.com>
2026-05-09 14:13:44 +0500

    updated email.ts file

--- src/services/email.ts
+++ src/services/email.ts
@@ -1,19 +1,19 @@
-import { Resend } from 'resend'
-import config from '../config/config'
+// import { Resend } from 'resend'
+// import config from '../config/config'
 
-const resend = new Resend(config.EMAIL_API_KEY)
+// const resend = new Resend(config.EMAIL_API_KEY)
 
-export default {
-    sendEmail: async (to: string[], subject: string, text: string) => {
-        try {
-            await resend.emails.send({
-                from: `Coderatory <onboarding@resend.dev>`,
-                to,
-                subject,
-                text
-            })
-        } catch (error) {
-            throw error
-        }
-    }
-}
+// export default {
+//     sendEmail: async (to: string[], subject: string, text: string) => {
+//         try {
+//             await resend.emails.send({
+//                 from: `Coderatory <onboarding@resend.dev>`,
+//                 to,
+//                 subject,
+//                 text
+//             })
+//         } catch (error) {
+//             throw error
+//         }
+//     }
+// }
